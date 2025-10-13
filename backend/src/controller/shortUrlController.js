9052dfc6ae6820befe1c3151b0802a67031058e9
Exploring-glitch <deysasha262@gmail.com>
2025-10-13 23:40:08 +0530

    structured the code

--- backend/src/controller/shortUrlController.js
+++ backend/src/controller/shortUrlController.js
@@ -4,6 +4,8 @@ import wrapAsync from "../utils/tryCatchWrapper.js";
 export const createShortUrl = wrapAsync( async (req, res, next) => { //CREATING THE SHORT URL
     const {url} = req.body;
     const ShortUrl = await createShortUrlWithoutUserService(url);
-    res.send(process.env.APP_URL + ShortUrl);
+    res.status(200).json({
+        result: process.env.APP_URL + ShortUrl
+    });
 })
 
--- frontend/src/api/shortUrlApi.js
+++ frontend/src/api/shortUrlApi.js
@@ -3,5 +3,5 @@ import { axiosInstance } from "../utils/axiosInstance.js";
 
 export const getShortUrlFromBackend = async (url) => {
     const {data} = await axiosInstance.post("api/create", {url})
-    return data;
+    return data.result; //we wrote .result because in controller we wrote result as key in json 
 }
