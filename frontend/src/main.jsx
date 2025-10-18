d93664ed30dedce101cd3fbcde135fede7de1ac3
Exploring-glitch <deysasha262@gmail.com>
2025-10-18 12:33:24 +0530

    setting up redux to the codebase

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,20 @@
+//Tanstack setup : routing folder
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient()
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routing/routeTree.js'
+import store from './store/store.js' //redux
 
 const router = createRouter({routeTree})
 
 createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+  <Provider store = {store}> 
+
+    <QueryClientProvider client={queryClient}> 
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+    
+  </Provider>
 )
