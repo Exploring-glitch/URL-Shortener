a0e903475d07c15e771b7bbb09f5e07755b1ff9c
Exploring-glitch <deysasha262@gmail.com>
2025-12-05 01:08:38 +0530

    changed the cookie options

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,6 +2,6 @@
 export const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
+    sameSite: "none",
     maxAge: 1000 * 60 * 60, // 1 hr
 }
