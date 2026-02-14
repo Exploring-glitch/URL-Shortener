b28f900a568882ef670e1d39fb2b37f2d83f9d29
Exploring-glitch <deysasha262@gmail.com>
2026-02-14 16:17:49 +0530

    updated the code

--- frontend/src/pages/DashBoardPage.jsx
+++ frontend/src/pages/DashBoardPage.jsx
@@ -4,13 +4,13 @@ import UserUrl from '../components/UserUrl'
 
 const DashBoardPage = () => {
   return (
-    <div className='min-h-screen bg-[#171717] flex flex-col items-center justify-center text-[#E0E0E0] p-3'> 
-        <div className='bg-[#1E1E1E] w-full max-w-4xl p-8 rounded-lg flex flex-col items-center'> 
-          <h1 className="text-2xl font-bold text-center mb-6">URL Shortener</h1>
+    <div className='min-h-screen bg-[#171717] flex flex-col items-center justify-center text-[#E0E0E0] p-3'>
+      <div className='bg-[#1E1E1E] w-full max-w-4xl p-8 rounded-lg flex flex-col items-center'>
+        <h1 className="text-2xl font-bold text-center mb-6">URL Shortener</h1>
 
-          <UrlForm />
-          <UserUrl />
-        </div>
+        <UrlForm />
+        <UserUrl />
+      </div>
     </div>
   )
 }
